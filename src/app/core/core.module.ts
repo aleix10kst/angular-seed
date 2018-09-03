dec706f05798ff98b1fb502590829f7e91d26dd2
Aleix Canet <acanet94@gmail.com>
2018-09-03 21:59:16 +0200

    Added HTTP client module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { CoreRoutingModule } from './core-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+    HttpClientModule,
     CoreRoutingModule
   ],
   exports: [
